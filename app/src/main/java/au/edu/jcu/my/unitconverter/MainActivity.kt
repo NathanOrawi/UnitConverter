a378b789071e118509cd1e54b379079d4c71e3bb
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2024-07-05 10:22:23 +1000

    add invertConversion() and comments

--- app/src/main/java/au/edu/jcu/my/unitconverter/MainActivity.kt
+++ app/src/main/java/au/edu/jcu/my/unitconverter/MainActivity.kt
@@ -12,28 +12,26 @@ class MainActivity : AppCompatActivity() {
             super.onCreate(savedInstanceState)
             binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
-            binding.calculateButton.setOnClickListener{ calculateConversion() }
+            // Sets the listener for the calculate button
+            binding.calculateButton.setOnClickListener { calculateConversion() }
+            // Sets the listener for the invert conversion switch
+            binding.roundUpSwitch.setOnCheckedChangeListener { _, _ -> invertConversion()}
         }
+
+    /**
+     * Calculates the conversion based on the selected radio button
+     */
     private fun calculateConversion() {
+        // Checks if the text field is empty
         val stringInTextField = binding.unitValueToConvert.text.toString()
+        // If the text field is empty, the conversion amount is set to empty
         val unitValue = stringInTextField.toDoubleOrNull()
         if (unitValue == null) {
-            binding.conversionAmount.text = ""
+            binding.conversionValue.text = ""
             return
         }
 
-        // Changes the text of the radio buttons based on the round up switch
-        if (binding.roundUpSwitch.isChecked) {
-            binding.optionCelsiusFahrenheit.text = getString(R.string.fahrenheit_to_celsius)
-            binding.optionDegreeRadians.text = getString(R.string.degrees_to_radians)
-            binding.optionKilometerMiles.text = getString(R.string.miles_to_kilometers)
-        } else {
-            binding.optionCelsiusFahrenheit.text = getString(R.string.celsius_to_fahrenheit)
-            binding.optionDegreeRadians.text = getString(R.string.radians_to_degrees)
-            binding.optionKilometerMiles.text = getString(R.string.kilometers_to_miles)
-        }
-
-        // Converts units based on the selected radio button, and the manner of conversion based on the round up switch
+        // Converts units based on the selected radio button
         val conversionPercentage: Double = when (binding.conversionOptions.checkedRadioButtonId) {
             // celsius to fahrenheit conversion
             R.id.option_celsius_fahrenheit -> if (binding.roundUpSwitch.isChecked) {
@@ -56,7 +54,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         val formattedConversion = NumberFormat.getInstance().format(conversionPercentage)
-        binding.conversionAmount.text = getString(R.string.conversion_amount, formattedConversion)
+        binding.conversionValue.text = getString(R.string.conversion_value, formattedConversion)
+    }
 
+    /**
+     * Inverts the conversion based on the invert conversion switch
+     */
+    private fun invertConversion() {
+        // Changes the text of the radio buttons based on the round up switch
+        if (binding.roundUpSwitch.isChecked) {
+            binding.optionCelsiusFahrenheit.text = getString(R.string.fahrenheit_to_celsius)
+            binding.optionDegreeRadians.text = getString(R.string.degrees_to_radians)
+            binding.optionKilometerMiles.text = getString(R.string.miles_to_kilometers)
+        } else {
+            binding.optionCelsiusFahrenheit.text = getString(R.string.celsius_to_fahrenheit)
+            binding.optionDegreeRadians.text = getString(R.string.radians_to_degrees)
+            binding.optionKilometerMiles.text = getString(R.string.kilometers_to_miles)
+        }
     }
 }
